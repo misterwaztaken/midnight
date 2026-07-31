4625aeb8a67ee884828a3f214d0efede68bac2e2
misterwaztaken <61604284+misterwaztaken@users.noreply.github.com>
2026-07-31 15:21:52 -0400

    scary git thing happened

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,14 +42,14 @@ android {
             )
         }
     }
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
-        }
-        kotlinOptions {
-            jvmTarget = "11"
-        }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
 
 dependencies {
 
